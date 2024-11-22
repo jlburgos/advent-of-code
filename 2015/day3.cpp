41913439ad4a76fa4e33b7e326bd1a55a179d8b0
Juan Burgos <juanlb1988@gmail.com>
2024-11-21 23:21:50 -0600

    Playing with the day3 solutions using slightly different implementations.

--- 2015/day3.cpp
+++ 2015/day3.cpp
@@ -2,6 +2,10 @@
 #include <array>
 #include <iostream>
 #include <string>
+#include <sstream>
+
+#include <unordered_map>
+#include <unordered_set>
 
 #include "libs/numeric-types.hpp"
 #include "libs/util.hpp"
@@ -9,10 +13,16 @@
 void part1(const std::vector<char>& input);
 void part2(const std::vector<char>& input);
 
+// Alternative routes to the same answer
+void part2_map(const std::vector<char>& input);
+void part2_set(const std::vector<char>& input);
+
 int main() {
   const std::vector<char> input = Util::getSingleLineInput("input/day3.dat");
   part1(input);
   part2(input);
+  part2_map(input);
+  part2_set(input);
   return 0;
 }
 
@@ -78,6 +88,84 @@ void part2(const std::vector<char>& input) {
   std::sort(houses.begin(), houses.end());
   const auto beginning_of_dupes = std::unique(houses.begin(), houses.end());
   houses.erase(beginning_of_dupes, houses.end());
+
+  std::cout << "Number of visited houses next year: " << houses.size() << std::endl;
+}
+
+void part2_map(const std::vector<char>& input) {
+  std::unordered_map<std::string, bool> houses;
+  houses.reserve(input.size());
+
+  const auto to_str = [](std::array<I32, 2> pos) {
+    std::stringstream ss;
+    ss << pos[0] << "_" << pos[1];
+    return ss.str();
+  };
+
+  std::array<I32, 2> santa = {0,0};
+  std::array<I32, 2> robot = {0,0};
+  houses.insert({to_str(santa), true}); // initial house
+
+  bool move_santa = true;
+  for (U32 i = 0; i < input.size(); ++i) {
+    std::array<I32, 2> move;
+    switch(input[i]) {
+      case '^': {move = {0,1}; break;}
+      case '>': {move = {1,0}; break;}
+      case 'v': {move = {0,-1}; break;}
+      case '<': {move = {-1,0}; break;}
+    }
+
+    if (move_santa) {
+      santa[0] += move[0];
+      santa[1] += move[1];
+      houses.insert({to_str(santa), true});
+    } else {
+      robot[0] += move[0];
+      robot[1] += move[1];
+      houses.insert({to_str(robot), true});
+    }
+    move_santa = !move_santa;
+  }
+
+  std::cout << "Number of visited houses next year: " << houses.size() << std::endl;
+}
+
+void part2_set(const std::vector<char>& input) {
+  std::unordered_set<std::string> houses;
+  houses.reserve(input.size());
+
+  const auto to_str = [](std::array<I32, 2> pos) {
+    std::stringstream ss;
+    ss << pos[0] << "_" << pos[1];
+    return ss.str();
+  };
+
+  std::array<I32, 2> santa = {0,0};
+  std::array<I32, 2> robot = {0,0};
+  houses.insert(to_str(santa)); // initial house
+
+  bool move_santa = true;
+  for (U32 i = 0; i < input.size(); ++i) {
+    std::array<I32, 2> move;
+    switch(input[i]) {
+      case '^': {move = {0,1}; break;}
+      case '>': {move = {1,0}; break;}
+      case 'v': {move = {0,-1}; break;}
+      case '<': {move = {-1,0}; break;}
+    }
+
+    if (move_santa) {
+      santa[0] += move[0];
+      santa[1] += move[1];
+      houses.insert(to_str(santa));
+    } else {
+      robot[0] += move[0];
+      robot[1] += move[1];
+      houses.insert(to_str(robot));
+    }
+    move_santa = !move_santa;
+  }
 
   std::cout << "Number of visited houses next year: " << houses.size() << std::endl;
 }
