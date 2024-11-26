5cbb8b6fc6e9eeae7246bb4b8382f1d57c6e0a77
Juan Burgos <juanlb1988@gmail.com>
2024-11-26 14:05:54 -0600

    AAdding day6 part2 solution along with some generics to remove code duplication.

--- 2015/day6.cpp
+++ 2015/day6.cpp
@@ -17,33 +17,30 @@ enum class Cmd {
 struct LightInstruction {
   Cmd cmd;
   std::array<std::array<U16, 2>, 2> coordinates;
-  LightInstruction(const Cmd _cmd,
-                   const std::array<U16, 2> coord1,
-                   const std::array<U16, 2> coord2)
-                   {
-                    cmd = _cmd;
-                    coordinates[0] = coord1;
-                    coordinates[1] = coord2;
-                   }
-  std::string_view toStringCmd() const {
-    switch(cmd) {
-      case Cmd::ON: return "ON";
-      case Cmd::OFF: return "OFF";
-      case Cmd::TOGGLE: return "TOGGLE";
-      default: {
-        std::cout << "Unsupported cmd encountered!" << std::endl;
-        exit(1);
-      }
-    }
+  LightInstruction(const Cmd _cmd, const std::array<U16, 2> coord1, const std::array<U16, 2> coord2) {
+    cmd = _cmd;
+    coordinates[0] = coord1;
+    coordinates[1] = coord2;
   }
+  std::string_view toStringCmd() const;
 };
 
+template<class ROW>
+struct Ops {
+  using LAMBDA = std::function<void(ROW &row, const U16 column)>;
+  const LAMBDA on_op;
+  const LAMBDA off_op;
+  const LAMBDA toggle_op;
 
-void part1(const std::vector<std::string>& input);
-void part2(const std::vector<std::string>& input);
+  // Takes instructions and applies operations on 'lights' vector elements
+  void apply(const std::vector<LightInstruction> &instructions, std::vector<ROW> &lights) const;
+};
+
+void part1(const std::vector<std::string> &input);
+void part2(const std::vector<std::string> &input);
 void printInstruction(const LightInstruction instruction);
 std::array<U16, 2> parseCoordinates(const std::string_view str);
-std::vector<LightInstruction> parseInput(const std::vector<std::string>& input);
+std::vector<LightInstruction> parseInput(const std::vector<std::string> &input);
 
 int main() {
   const std::vector<std::string> input = Util::getMultiLineInput("input/day6.dat");
@@ -77,7 +74,7 @@ std::array<U16, 2> parseCoordinates(const std::string_view str) {
   return coordinates;
 }
 
-std::vector<LightInstruction> parseInput(const std::vector<std::string>& input) {
+std::vector<LightInstruction> parseInput(const std::vector<std::string> &input) {
   const std::string cmds[3] = {"toggle", "turn on", "turn off"};
   const std::string digits = "0123456789";
 
@@ -116,39 +113,35 @@ std::vector<LightInstruction> parseInput(const std::vector<std::string>& input) 
   return instructions;
 }
 
-void part1(const std::vector<std::string>& input) {
-  // Settings some configurations
-  const U16 num_columns = 1000;
-  using ROW = std::bitset<num_columns>;
-  using LAMBDA = std::function<void(ROW &row, const U16 column)>;
 
-  const std::vector<LightInstruction> instructions = parseInput(input);
-  std::vector<ROW> lights;
-  lights.resize(num_columns); // initializes 1000 columns with bit value 'false'
+std::string_view LightInstruction::toStringCmd() const {
+  switch(cmd) {
+    case Cmd::ON: return "ON";
+    case Cmd::OFF: return "OFF";
+    case Cmd::TOGGLE: return "TOGGLE";
+    default: {
+      std::cout << "Unsupported cmd encountered!" << std::endl;
+      exit(1);
+    }
+  }
+}
 
-  // Create lambdas that store the operations that need to be done
+template<class ROW>
+void Ops<ROW>::apply(const std::vector<LightInstruction> &instructions, std::vector<ROW> &lights) const {
   const LAMBDA *op;
-  const LAMBDA ON_OP     = [](ROW &row, const U16 column) { row.set(column); };
-  const LAMBDA OFF_OP    = [](ROW &row, const U16 column) { row.reset(column); };
-  const LAMBDA TOGGLE_OP = [](ROW &row, const U16 column) { row.flip(column); };
-
-  // Collect coordinates in convenient arrays
-  std::array<U16, 2> xrange;
-  std::array<U16, 2> yrange;
-
   for (const LightInstruction instruction : instructions) {
     // Set lambda operation depending on the parsed command
     switch(instruction.cmd) {
       case Cmd::OFF: {
-        op = &OFF_OP;
+        op = &off_op;
         break;
       }
       case Cmd::ON: {
-        op = &ON_OP;
+        op = &on_op;
         break;
       }
       case Cmd::TOGGLE: {
-        op = &TOGGLE_OP;
+        op = &toggle_op;
         break;
       }
       default: {
@@ -159,23 +152,56 @@ void part1(const std::vector<std::string>& input) {
 
     // Since we're iterating through a vector of bitsets, coordinates system is actually (y,x)
     // where "y" points to a row in the vector and "x" points to a column in the bitset.
-    xrange[0] = instruction.coordinates[0][0];
-    xrange[1] = instruction.coordinates[1][0];
-    yrange[0] = instruction.coordinates[0][1];
-    yrange[1] = instruction.coordinates[1][1];
+    const std::array<U16, 2> xrange = {instruction.coordinates[0][0], instruction.coordinates[1][0]};
+    const std::array<U16, 2> yrange = {instruction.coordinates[0][1], instruction.coordinates[1][1]};
     for (U16 y = yrange[0]; y <= yrange[1]; ++y) {
       for (U16 x = xrange[0]; x <= xrange[1]; ++x) {
         (*op)(lights[y], x);
       }
     }
   }
+}
 
-  const auto sumRow = [](std::size_t total, const ROW& row) -> std::size_t { return total + row.count(); };
+void part1(const std::vector<std::string> &input) {
+  // Settings some configurations
+  const U16 num_columns = 1000;
+  using ROW = std::bitset<num_columns>;
+
+  const std::vector<LightInstruction> instructions = parseInput(input);
+  std::vector<ROW> lights;
+  lights.resize(num_columns); // initializes 1000 rows of columns with bit value '0'
+
+  // Create lambdas that store the operations that need to be applied
+  const Ops<ROW> ops = {
+    [](ROW &row, const U16 column) { row.set(column); },
+    [](ROW &row, const U16 column) { row.reset(column); },
+    [](ROW &row, const U16 column) { row.flip(column); }
+  };
+  ops.apply(instructions, lights);
+
+  const auto sumRow = [](std::size_t total, const ROW &row) -> std::size_t { return total + row.count(); };
   const std::size_t count = std::accumulate(lights.begin(), lights.end(), 0, sumRow);
-
   std::cout << "(Part 1) There are " << count << " lights that are lit." << std::endl;
 }
 
-void part2(const std::vector<std::string>& input) {
-  // TODO
+void part2(const std::vector<std::string> &input) {
+  // Settings some configurations
+  const U16 num_columns = 1000;
+  using ROW = std::array<U16, num_columns>;
+
+  const std::vector<LightInstruction> instructions = parseInput(input);
+  std::vector<ROW> lights;
+  lights.resize(num_columns); // initializes 1000 rows of columns with integral value '0'
+
+  // Create lambdas that store the operations that need to be applied
+  const Ops<ROW> ops = {
+    [](ROW &row, const U16 column) { row[column] += 1; },
+    [](ROW &row, const U16 column) { row[column] = (row[column] == 0 ? 0 : row[column] - 1); },
+    [](ROW &row, const U16 column) { row[column] += 2; }
+  };
+  ops.apply(instructions, lights);
+
+  const auto sumRow = [](std::size_t total, const ROW &row) -> std::size_t { return total + std::accumulate(row.begin(), row.end(), 0); };
+  const std::size_t brightness = std::accumulate(lights.begin(), lights.end(), 0, sumRow);
+  std::cout << "(Part 2) Total brightness of lit lights is " << brightness << std::endl;
 }
